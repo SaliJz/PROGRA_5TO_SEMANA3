8a98e1b746ea8844a4c8a52aee452519ff3f9993
Joaquin <166671976+SaliJz@users.noreply.github.com>
2026-09-10 22:05:29 -0500

    Feat: Interface ICombatant + Abstract class Character

---
+++ Turn_Based_Game/Character.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turn_Based_Game
+{
+    public abstract class Character : ICombatant
+    {
+        public int Health { get; protected set; }
+        public int Damage { get; protected set; }
+
+        protected Character(int health, int damage)
+        {
+            if (health <= 0 || health > 100)
+            {
+                throw new ArgumentException("La vida debe ser mayor que 0 y no superar 100.");
+            }
+
+            if (damage < 0 || damage > 100)
+            {
+                throw new ArgumentException("El daño debe estar entre 0 y 100.");
+            }
+
+            Health = health;
+            Damage = damage;
+        }
+
+        public virtual void TakeDamage(int damage)
+        {
+            if (damage < 0)
+            {
+                throw new ArgumentException("El daño no puede ser negativo.");
+            }
+
+            Health -= damage;
+
+            if (Health < 0) Health = 0;
+        }
+
+        public virtual int GetDamage()
+        {
+            return Damage;
+        }
+
+        public virtual bool IsAlive()
+        {
+            return Health > 0;
+        }
+    }
+}
