259e4edb2408708a3f8065fde21c10abd1dc9ef6
Dati-S2 <damaris852@gmail.com>
2026-09-17 14:49:00 -0500

    Add Player

---
+++ Turn_Based_Game/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turn_Based_Game
+{
+    internal class Player : Character
+    {
+        public Player(int health, int damage) : base(health, damage)
+        {
+        }
+    }
+}
