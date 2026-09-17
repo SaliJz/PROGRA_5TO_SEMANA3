04c189c1c4ceeb0111ee5a2589b2dd4bfac3420f
T4c0-PT <danielpp1801@gmail.com>
2026-09-17 15:03:24 -0500

    Add Enemy

---
+++ Turn_Based_Game/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp2.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public abstract class Enemy : Character
+    {
+        public string Name { get; }
+        public Enemy(int health, int damage, string name) : base(health, damage)
+        {
+            Name = name;
+        }
+    }
+}
