936e1164c362e4c8ac4c844c9a04e7dff2caf080
Belen Castrillon Peche <brooksamy137@gmail.com>
2026-09-22 13:09:20 -0500

    Script Enemigo Melee

---
+++ Belen/Enemigo_Melee/Enemigo_Melee/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enemigo_Melee
+{
+    internal class Program
+    {
+        private int vida;
+        private int daño;
+        
+        public void EnemigoMelee(int vidaInicial, int dañoInicial)
+        {
+            vida = vidaInicial;
+            daño = dañoInicial;
+        }
+
+        public void RecibirDaño(int dañoRecibido)
+        {
+            vida -= dañoRecibido;
+
+            if(vida < 0)
+            {
+                vida = 0;
+            }
+        }
+
+        public int DañoRealizado()
+        {
+            return daño;
+        }
+
+        public bool EstaVivo()
+        {
+            return vida > 0;
+        }
+
+        public int VidaRestante()
+        {
+            return vida;
+        }
+    }
+}
